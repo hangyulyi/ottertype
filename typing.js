6cf65e13d33e5b3406637ce61355ecf4d96677ac
Hangyul Yi <hangyulyi3@gmail.com>
2024-02-16 23:02:33 -0700

    initial commit

---
+++ typing.js
@@ -0,0 +1,11 @@
+const words = [];
+
+/* Function to load words from  */
+
+const wordsCount = words.length;
+const gameTime = 30 * 1000;
+
+window.timer = null;
+window.gameStart = null;
+window.pauseTime = 0;
+
