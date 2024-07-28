159f96a02998a626d27ba0534991162e63ed0708
Hangyul Yi <hangyulyi3@gmail.com>
2024-07-28 01:09:45 -0600

    add icons and labels in footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,27 @@
+import { TbMailFilled } from "react-icons/tb";
+import { LuCode2 } from "react-icons/lu";
+
 const Footer = () => {
     return (
-        <footer>
+        <footer className="text-sub-color py-4">
+            <div className="container mx-auto flex justify-start space-x-4">
 
+                <a
+                    href="https://github.com/hangyulyi/ottertype"
+                    className="flex items-center space-x-1 hover:text-text-color"
+                >
+                    <TbMailFilled />
+                    <span>contact</span>
+                </a>
+
+                <a
+                    href="https://github.com/hangyulyi/ottertype"
+                    className="flex items-center space-x-1 hover:text-text-color"
+                >
+                    <LuCode2 />
+                    <span>github</span>
+                </a>
+            </div>
         </footer>
     )
 }
