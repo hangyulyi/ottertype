271dc05d76376bc6fc0db06ef1af0d55a745684b
Hangyul Yi <hangyulyi3@gmail.com>
2024-07-30 22:56:23 -0600

    fix font in header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,16 @@
 import Image from "next/image";
 
-import { Nunito_Sans } from "next/font/google";
+import { Jost } from "next/font/google";
 
-const nunito_sans = Nunito_Sans({
-    subsets: ['latin'],
-    weight: '500'
+const jost = Jost({
+    subsets: ['latin']
 })
 
 const Header = () => {
     return (
         <header className="p-10 flex">
             <Image src={`/pixelotter.png`} alt="Logo" width={50} height={50} />
-            <h1 className={`${nunito_sans.className} font-logo text-text-color text-4xl`}>ottertype</h1>
+            <h1 className={`${jost.className} font-logo text-text-color text-4xl tracking-wide font-medium`}>ottertype</h1>
         </header>
     )
 }
